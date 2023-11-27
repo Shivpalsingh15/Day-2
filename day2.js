f0e848dc7a1ba71a6a5f6999630d579ba090facd
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2023-11-27 15:13:26 +0530

    Operators basic

---
+++ day2.js
@@ -0,0 +1,112 @@
+// 1) Linking js file.
+/*
+<script src = "index.js"></script>
+
+2) Template literals
+
+It is Syntax for writing string in another way.
+Syntax is that it start with back ticks and if we want to write a variable inside it we are $ symbol follwed with curly braces and in curly braces our variable name.
+
+
+let a = 5;
+let b = 8;
+console.log(`Output of a+b: ${a+b}`)
+
+
+let fname = 'robin';
+// console.log('fname:', fname)
+let lastName = 'uthapa';
+console.log(`cricketer ${fname} ${lastName}`);
+
+3) Comaparison Operator.
+A comparison operator compares two values or expressions.
+It gives boolean value on comparing values.
+
+let a = 5;
+let b = 8;
+
+console.log(a!=b);
+
+
+let a = 5;
+let b = 8;
+
+console.log(a == b); //True output.
+
+
+let a = 5;
+let b = 8;
+
+console.log(a != b);
+
+//Comparison operator.
+
+// let a = 5
+// let b = 6
+// console.log(5>6)
+
+// let a = 5;
+// let b = 6;
+// console.log(5 == 6);
+
+4) Conditional statement.
+
+if else
+nested 
+switch condition
+*/
+
+
+let a = 50
+if (a>=15){
+    console.log("yes")
+}else{
+    console.log("no")
+}
+
+let age = 15;
+if(age <=12){
+    console.log("baccha.")
+}else{
+    console.log("bada log.")
+}
+
+// //while loops
+
+// const n= 0;
+// while(n<3){
+//     console.log("hey")
+//     // console.log(n)
+//     n++
+// }
+
+// const ane  = 'namr'
+// ane='sxcds'
+
+// console.log(ane)
+
+// alert("Hii")
+// let age = 25;
+
+// switch (true) {
+//   case age <= 18:
+//     console.log("small or equal to 18");
+//     break;
+//   case age > 18:
+//     console.log("must be bigger than 18 age");
+//     break;
+//   default:
+//     console.log("kuch aur ki picture hai ");
+// }
+
+//promt
+
+// alert("heu")
+// console.log(alert("new pafe"))
+
+// let firstName = prompt("Enter FN");
+// let lastName = prompt("Enter LN");
+
+// document.write(`${firstName} ${lastName}`);
+
+
